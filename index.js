5e38717f435ac3895b004fabad302c0dc96dbfb2
SENZUBEANs <gametechcluster@gmail.com>
2024-11-16 19:11:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import App from "./App.js"; // Note the .js extension
 import "./styles.css";
 
 const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
+const root = ReactDOM.createRoot(rootElement);
 
 root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  React.createElement(React.StrictMode, null,
+    React.createElement(App)
+  )
 );
